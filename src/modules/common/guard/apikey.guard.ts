76d3c75b2aaa47d34f8639c5b15e15b4f942d15a
NguyenDucTam <tam9898980@gmail.com>
2025-12-25 01:04:37 +0700

    chore: add mode on/off for api key guard

--- src/modules/common/guard/apikey.guard.ts
+++ src/modules/common/guard/apikey.guard.ts
@@ -5,6 +5,11 @@ import { ConfigService } from '@nestjs/config';
 export class ApiKeyGuard implements CanActivate {
   constructor(private readonly configService: ConfigService) {}
   canActivate(context: ExecutionContext): boolean {
+    const apiKeyActivation =
+      this.configService.getOrThrow<string>('API_KEY_ACTIVATION');
+    if (apiKeyActivation !== 'true') {
+      return true;
+    }
     const request = context.switchToHttp().getRequest<Request>();
     const apiKeyFromRequest = request.headers['x-api-key'] as string;
     const validApiKey = this.configService.getOrThrow<string>('API_KEY');
