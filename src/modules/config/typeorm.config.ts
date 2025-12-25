e804c00b0f818ac781a7b3a46ffca139b7977b75
NguyenDucTam <tam9898980@gmail.com>
2025-12-25 08:34:02 +0700

    chore: set synchronize in typeorm

--- src/modules/config/typeorm.config.ts
+++ src/modules/config/typeorm.config.ts
@@ -8,6 +8,6 @@ export const typeORMConfig = (
   type: 'postgres',
   url: configService.getOrThrow<string>('DB_URL'),
   // logging: ['error', 'warn', 'query'],
-  synchronize: process.env.NODE_ENV === 'development' ? true : false,
+  synchronize: true,
   entities: [path.join(__dirname, '..', 'entities', '*.entity{.ts,.js}')],
 });
