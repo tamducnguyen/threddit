92a8dcd933cc51c6ddf131166dcbdd6120a94be9
NguyenDucTam <tam9898980@gmail.com>
2026-01-05 08:23:02 +0700

    add api docs for v2 notification module

--- src/modules/notification/notification.controller.ts
+++ src/modules/notification/notification.controller.ts
@@ -24,7 +24,6 @@ import {
   ApiBadRequestResponse,
   ApiBearerAuth,
   ApiCookieAuth,
-  ApiNoContentResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
   ApiOperation,
@@ -35,6 +34,7 @@ import {
   ApiTooManyRequestsResponse,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
+
 @ApiTags('Notification')
 @ApiBearerAuth()
 @ApiCookieAuth()
@@ -53,12 +53,13 @@ import {
 @SkipThrottle({ public: true })
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
+
   @Sse('listen')
   @ApiOperation({
     summary: 'Lắng nghe thông báo thời gian thực (SSE)',
     description: `Thiết lập kết nối **Server-Sent Events** để nhận thông báo theo thời gian thực. **Lắng nghe ngay khi người dùng vào hệ thống**. 
       Kết nối được giữ mở và máy chủ sẽ chủ động đẩy sự kiện khi có thông báo mới thuộc về người dùng hiện tại.
-      Mỗi sự kiện chứa thông tin của một thông báo (id, thời điểm tạo, trạng thái đã đọc và nội dung).`,
+      Mỗi sự kiện chứa thông tin của một thông báo (id, thời điểm tạo, trạng thái đã đọc, những thông tin đối tượng thông báo và thông điệp).`,
   })
   @ApiProduces('text/event-stream')
   @ApiOkResponse({
@@ -67,7 +68,15 @@ export class NotificationController {
       Kết nối sẽ trả về các sự kiện có định dạng sau:
       Chỉ bắt data
       id: 1
-      data: {"id":21,"createdAt":"2025-10-09T14:06:21.226Z","type":"follow","target":"tammmmm1","isRead":false,"content":"tammmmm1 đã bắt đầu theo dõi bạn"}      Luồng sẽ tiếp tục gửi thêm các sự kiện mới cho đến khi người dùng ngắt kết nối.`,
+      data: {"id":21,"createdAt":"2025-10-09T14:06:21.226Z","type":"follow","target":{
+              type: 'FRIEND_CONTENT_CREATION',
+              contentId: 1,
+              contentType: friend_content_creation,
+              "actorUsername": "tamducnguyentest1",
+              "actorAvatarUrl": "https://threddit-s3.s3.ap-southeast-1.amazonaws.com/avatar/default_avatar.jpg",
+              "actorDisplayName": "Nguyễn Đức Tâm"
+            },"isRead":false,"message":"tammmmm1 đã bắt đầu theo dõi bạn"}      
+      Luồng sẽ tiếp tục gửi thêm các sự kiện mới cho đến khi người dùng ngắt kết nối.`,
   })
   @ApiBadRequestResponse({
     description:
@@ -76,6 +85,7 @@ export class NotificationController {
   async createStream(@CurrentUser() currentUser: AuthUser) {
     return await this.notificationService.createStream(currentUser);
   }
+
   @HttpCode(HttpStatus.OK)
   @Get()
   @ApiOperation({
@@ -84,7 +94,7 @@ export class NotificationController {
       '**Trả về danh sách thông báo của người dùng hiện tại theo thứ tự gần đây nhất. ' +
       'Hỗ trợ phân trang tiến bằng một mã con trỏ nằm trên query string. ' +
       'Người dùng kéo hết thì gọi api tiếp kèm theo cursor' +
-      'Nếu không còn dữ liệu để trả về, máy chủ có thể trả trạng thái không có nội dung.**',
+      'Nếu không còn dữ liệu để trả về, máy chủ trả về mảng rỗng và con trỏ là null.**',
   })
   @ApiOkResponse({
     description:
@@ -93,9 +103,6 @@ export class NotificationController {
   @ApiBadRequestResponse({
     description:
       'Thất bại: người dùng không hợp lệ hoặc mã con trỏ không hợp lệ',
-  })
-  @ApiNoContentResponse({
-    description: 'Không có thông báo / Đã lấy hết thông báo',
   })
   @ApiQuery({
     name: 'cursor',
@@ -112,6 +119,7 @@ export class NotificationController {
       cursorDTO?.cursor,
     );
   }
+
   @HttpCode(HttpStatus.OK)
   @Get('unread')
   @ApiOperation({
@@ -120,7 +128,7 @@ export class NotificationController {
       '**Trả về danh sách thông báo chưa đọc của người dùng hiện tại theo thứ tự gần đây nhất. ' +
       'Hỗ trợ phân trang tiến bằng một mã con trỏ nằm trên query string. ' +
       'Người dùng kéo hết thì gọi api tiếp kèm theo cursor' +
-      'Nếu không còn dữ liệu để trả về, máy chủ có thể trả trạng thái không có nội dung.**',
+      'Nếu không còn dữ liệu để trả về, máy chủ trả về mảng rỗng và con trỏ là null .**',
   })
   @ApiOkResponse({
     description:
@@ -129,9 +137,6 @@ export class NotificationController {
   @ApiBadRequestResponse({
     description:
       'Thất bại: người dùng không hợp lệ hoặc mã con trỏ không hợp lệ',
-  })
-  @ApiNoContentResponse({
-    description: 'Không có thông báo / Đã lấy hết thông báo **chưa đọc**',
   })
   @ApiQuery({
     name: 'cursor',
@@ -148,6 +153,7 @@ export class NotificationController {
       cursorDTO?.cursor,
     );
   }
+
   @HttpCode(HttpStatus.OK)
   @Post(':notificationId/read')
   @ApiOperation({ summary: 'Chuyển trạng thái thông báo thành đã đọc.' })
@@ -172,8 +178,22 @@ export class NotificationController {
       readnotifDTO.notificationId,
     );
   }
+
   @HttpCode(HttpStatus.OK)
   @Delete(':notificationId')
+  @ApiOperation({ summary: 'Xóa thông báo theo id của thông báo.' })
+  @ApiNotFoundResponse({
+    description: 'Không tìm thấy thông báo.',
+  })
+  @ApiOkResponse({
+    description: 'Xóa thông báo thành công.',
+  })
+  @ApiParam({
+    name: 'notificationId',
+    required: true,
+    description: 'ID của thông báo.',
+    type: String,
+  })
   async deleteNotification(
     @CurrentUser('sub') sub: number,
     @Param() deleteNotificationDTO: DeleteNotificationDTO,
@@ -183,6 +203,7 @@ export class NotificationController {
       deleteNotificationDTO.notificationId,
     );
   }
+
   @HttpCode(HttpStatus.OK)
   @Get('count/unread')
   @ApiOperation({ summary: 'Lấy số lượng thông báo chưa đọc.' })
@@ -191,8 +212,13 @@ export class NotificationController {
   async getCountUnreadNotification(@CurrentUser('sub') id: number) {
     return await this.notificationService.getCountUnreadNotificationount(id);
   }
+
   @HttpCode(HttpStatus.OK)
   @Post('readall')
+  @ApiOperation({ summary: 'Đánh dấu tất cả thông báo là đã đọc' })
+  @ApiOkResponse({
+    description: 'Đánh dấu tất cả thông báo là đã đọc thành công',
+  })
   async readAllNotifications(@CurrentUser() currentUser: AuthUser) {
     return await this.notificationService.readAllNotifications(currentUser);
   }
